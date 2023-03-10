ea971dd1b4a8d9b945af3cfbd7a5818cb7d97063
Gaurav Tyagi <118004334+Gaurav-Tyagi-cse@users.noreply.github.com>
2023-03-10 20:51:38 +0530

    Add files via upload

---
+++ Array/Second_mex.java
@@ -0,0 +1,34 @@
+public class Second_mex 
+{
+   
+    private static void secondm(int[] array,int length) 
+    {
+        // time complexity is O(n)
+         int higest=Integer.MIN_VALUE;
+         int secondhighest=Integer.MIN_VALUE;
+         for(int i=0; i<length; i++)
+         {
+            if(array[i]>higest)
+            {
+                secondhighest=higest;
+                higest=array[i];
+            }
+            if(array[i]<higest && array[i]>secondhighest)
+            {
+                secondhighest=array[i];
+            }
+         }
+         
+         System.out.println("Second highest is "+secondhighest); 
+    }
+    public static void main(String[] args) 
+    {
+        int array[]={2,7,11,15, 2,14,6,3 };
+        int length=array.length;
+        secondm(array,length);
+        
+       
+    }
+
+    
+}
