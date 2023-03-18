39010f00e9c1ffbd2af66667ba73941683d3b6b5
Gaurav Tyagi <118004334+Gaurav-Tyagi-cse@users.noreply.github.com>
2023-03-18 14:45:22 +0530

    Add files via upload

---
+++ GasStation.java
@@ -0,0 +1,25 @@
+public class GasStation {
+    public static void main(String[] args) {
+        int gas[] = {1,2,3,4,5};
+        int cost[] = {3,4,5,1,2};
+        int rem = 0;
+        int start =-1;
+        int loss=-1;
+        for (int i = 0; i <gas.length; i++) {
+            rem+=gas[i]-cost[i];
+            if(rem<0)
+            {
+                start = i+1;
+                loss+=rem;
+                rem = 0;
+                
+            }
+        }
+        if (loss + rem >=0) {
+           // System.out.println(start);
+           
+        }
+        System.out.println(start);
+        
+    }
+}
